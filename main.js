fabdd67519f6d4da6dd90fb67b0b9cc60b2baf8b
Devam <devam433@gmail.com>
2023-09-12 23:58:39 +0530

    Modefied the image source

--- main.js
+++ main.js
@@ -8,15 +8,15 @@ function diceRoll(){
 
     let image1=document.querySelectorAll("img")[0]
     let randomDiceImage="dice"+rNumber+".png"
-    let randomDiceSource="images/"+randomDiceImage
-    image1.setAttribute("src",randomDiceSource)
+    // let randomDiceSource="images/"+randomDiceImage
+    image1.setAttribute("src",randomDiceImage)
 
 //second dice
 
     let image2=document.querySelectorAll("img")[1]
     let randomDiceImage2="dice"+rNumber2+".png"
-    let randomDiceSource2="images/"+randomDiceImage2
-    image2.setAttribute("src",randomDiceSource2)
+    // let randomDiceSource2="images/"+randomDiceImage2
+    image2.setAttribute("src",randomDiceImage2)
 
     if(rNumber>rNumber2)
     {
