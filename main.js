4db3dac013e9d5b0cf2ce224d6055ad77fab7a64
Devam <devam433@gmail.com>
2023-09-14 17:27:34 +0530

    Optimize the app

--- main.js
+++ main.js
@@ -1,36 +1,34 @@
+/*Here I have initialized the following 3 variables outside the diceRoll function because there is no need
+of initializing these variables with each function call. Instead of repeatedly selecting the same DOM elements 
+inside the diceRoll function, we can select them once outside the function and reuse the references. 
+This can improve performance by reducing the number of DOM queries.
+*/
+let verdictElement = document.getElementById("verdict");
+let imageElement = document.querySelectorAll("img");
+let refreshBtn = document.getElementById("btn");
 
+function randomNumberGenerator() {
+    return Math.floor(Math.random() * 6) + 1;
+}
+function diceRoll() {
+    refreshBtn.innerText = "Roll";
+    let rNumber = randomNumberGenerator()
+    let rNumber2 = randomNumberGenerator()
 
-function diceRoll(){  
-    document.getElementById("btn").innerText="Refresh"
-    let rNumber= Math.floor(Math.random()*6)+1
-    let rNumber2= Math.floor(Math.random()*6)+1
-//first dice
+    // First dice
+    let randomDiceImage = "dice" + rNumber + ".png";
+    imageElement[0].setAttribute("src", randomDiceImage);
 
-    let image1=document.querySelectorAll("img")[0]
-    let randomDiceImage="dice"+rNumber+".png"
-    // let randomDiceSource="images/"+randomDiceImage
-    image1.setAttribute("src",randomDiceImage)
+    // Second dice
+    let randomDiceImage2 = "dice" + rNumber2 + ".png";
+    imageElement[1].setAttribute("src", randomDiceImage2)
 
-//second dice
-
-    let image2=document.querySelectorAll("img")[1]
-    let randomDiceImage2="dice"+rNumber2+".png"
-    // let randomDiceSource2="images/"+randomDiceImage2
-    image2.setAttribute("src",randomDiceImage2)
-
-    if(rNumber>rNumber2)
-    {
-        document.getElementById("verdict").innerText="Player1 wins!"
+    //Winner logic
+    if (rNumber > rNumber2) {
+        verdictElement.innerText = "Player1 wins!";
+    } else if (rNumber < rNumber2) {
+        verdictElement.innerText = "Player2 wins!";
+    } else {
+        verdictElement.innerText = "Draw";
     }
-    else if(rNumber<rNumber2)
-    {
-        document.getElementById("verdict").innerText="Player2 wins!"
-    }
-    else{
-        document.getElementById("verdict").innerText="Draw"
-    } 
-    document.getElementById("btn").innerText="Play again"
-    diceRoll()
-    }
-
-    
+}
